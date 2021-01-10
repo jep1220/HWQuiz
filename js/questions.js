ff1a73838fd14a8cfffb4e8d8d722c5be8652789
Ernie Perez <ernieperez96@gmail.com>
2021-01-09 23:26:05 -0800

    js questions created

---
+++ js/questions.js
@@ -0,0 +1,36 @@
+// questions, choices, and answers
+var questions = [
+    {
+      title: "The name of Han Solo's ship is:",
+      choices: ["Centurion Firebird", "Millenium Falcon", "USS Enterprise", "Chewbacca"],
+      answer: "Millenium Falcon"
+    },
+    {
+      title: "_____ built C-3P0",
+      choices: ["R2D2", "Princess Leia", "Chewbacca", "Anakin Skywalker"],
+      answer: "Anakin Skywalker"
+    },
+    {
+      title: "Who is Chancellor Palpatine",
+      choices: [
+        "The senate",
+        "Jedi Master",
+        "Anakin's Father",
+        "The chosen one"
+      ],
+      answer: "The senate"
+    },
+    {
+      title:
+        "Who shot first",
+      choices: ["Greedo", "Princess Leia", "Han Solo", "Boba Fett"],
+      answer: "Han Solo"
+    },
+    {
+      title:
+        "How did Obi-Wan beat Vader",
+      choices: ["Months of planning", "Deceipt", "He had the high ground", "he didn't beat Vader"],
+      answer: "He had the high ground"
+    }
+  ];
+  
